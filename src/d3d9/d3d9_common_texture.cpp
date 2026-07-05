58981f9cdb0724b022746efa742eb642d364841d
DevPark93 <devpark93@gmail.com>
2026-07-05 15:59:27 +0900

    d3d9: allow sampled view type mismatch for Mabinogi test

--- src/d3d9/d3d9_common_texture.cpp
+++ src/d3d9/d3d9_common_texture.cpp
@@ -717,7 +717,7 @@ namespace dxvk {
     viewInfo.aspects   = lookupFormatInfo(viewInfo.format)->aspectMask;
     viewInfo.usage     = UsageFlags;
     viewInfo.viewType  = GetImageViewTypeFromResourceType(m_type, Layer);
-    viewInfo.allowTypeMismatch = !(UsageFlags & VK_IMAGE_USAGE_SAMPLED_BIT);
+    viewInfo.allowTypeMismatch = true;
     viewInfo.mipIndex  = Lod;
     viewInfo.mipCount  = m_desc.MipLevels - Lod;
     viewInfo.layerIndex = Layer == AllLayers ? 0 : Layer;
